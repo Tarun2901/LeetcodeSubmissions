1562fa655b28d9ec51b44bf424b0885d1e17ce51
Tarun Yadav <tarunrock2901@gmail.com>
2021-05-19 12:17:40 +0530

    Time: 12 ms (93.55%), Space: 10 MB (28.58%) - LeetHub

--- number-of-islands/number-of-islands.cpp
+++ number-of-islands/number-of-islands.cpp
@@ -1,37 +1,41 @@
 class Solution {
 public:
-     void dfs(vector<vector<char>>& grid, vector<vector<bool>>& visited, int x, int y, int rows, int cols){
-        if(x >= rows || x < 0 || y >= cols || y < 0 || grid[x][y] == '0' || visited[x][y]){
+    
+    void dfs(vector<vector<char>>&grid,int x,int y,int row, int col,vector<vector<int>>& visited)
+    {   
+        //Base case
+        if(x<0||x>=row||y<0||y>=col||grid[x][y]=='0'||visited[x][y]==1)
+        {
             return;
         }
-        visited[x][y] = true;
-        dfs(grid, visited, x + 1, y, rows, cols);
-        dfs(grid, visited, x, y + 1, rows, cols);
-        dfs(grid, visited, x - 1, y, rows, cols);
-        dfs(grid, visited, x, y - 1, rows, cols);                
+        //4 calls
+        visited[x][y] = 1;
+        dfs(grid,x,y-1,row,col,visited);
+        dfs(grid,x,y+1,row,col,visited);
+        dfs(grid,x+1,y,row,col,visited);
+        dfs(grid,x-1,y,row,col,visited);
     }
     
     int numIslands(vector<vector<char>>& grid) {
-           if(grid.size() == 0)
-           {
-               return 0;
-           }
-        
-           int rows = grid.size();
-           int col = grid[0].size();
-           vector<vector<bool>> visited(rows,vector<bool>(col,false));
-           int count = 0;
-           for(int i = 0;i<rows;i++)
-           {
-               for(int j = 0;j<col;j++)
-               {
-                   if(!visited[i][j]&&grid[i][j]=='1')
-                   {
-                       count++;
-                       dfs(grid,visited,i,j,rows,col);
-                   }
-               }
-           }
-           return count;
+        if(grid.size() == 0)
+        {
+            return 0;
+        }
+        int row = grid.size();
+        int col = grid[0].size();
+        vector<vector<int>>visited(row,vector<int>(col,0));
+        int count = 0;
+        for(int i = 0;i<row;i++)
+        {
+            for(int j = 0;j<col;j++)
+            {
+                if(visited[i][j] == 0 && grid[i][j] == '1')
+                {
+                  dfs(grid,i,j,row,col,visited);
+                  count++;
+                }
+            }
+        }
+        return count;
     }
 };
