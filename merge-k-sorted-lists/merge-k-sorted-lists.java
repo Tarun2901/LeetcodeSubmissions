56ed127dc62b881a57581bc4611508a47b2d6948
Tarun Yadav <tarunrock2901@gmail.com>
2021-05-21 13:45:03 +0530

    Time: 2 ms (88.13%), Space: 44.4 MB (5.05%) - LeetHub

---
+++ merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+       public ListNode merge2SLL(ListNode l1,ListNode l2)
+    {   
+        if(l1==null||l2==null)
+        {
+            return l1!=null?l1:l2;
+        }
+        ListNode c1 = l1;
+        ListNode c2 = l2;
+        ListNode dummy = new ListNode(-1);
+        ListNode prev = dummy;
+        while(c1!=null&&c2!=null)
+        {
+            if(c1.val<c2.val)
+            {
+                prev.next = c1;
+                c1 = c1.next;
+            }
+            else
+            {
+                prev.next = c2;
+                c2 = c2.next;
+            }
+            prev = prev.next;
+        }
+        prev.next = c1!=null?c1:c2;
+        return dummy.next;
+    }
+    public ListNode solve(ListNode[] lists,int si,int ei)
+    {
+        if(si>ei)
+        {
+            return null;
+        }
+        if(si == ei)
+        {   //return the list present at that particular idx
+            return lists[si];
+        }
+        int mid = (si+ei)/2;
+        ListNode l1 = solve(lists,si,mid);
+        ListNode l2 = solve(lists,mid+1,ei);
+        return merge2SLL(l1,l2);
+    }
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists.length==0)
+        {
+            return null;
+        }
+        return solve(lists,0,lists.length-1);
+    }
+}
