83f43d376f83ef287576ab35972c79dd61ccf978
Tarun Yadav <tarunrock2901@gmail.com>
2021-06-18 18:21:11 +0530

    Time: 190 ms (89.21%), Space: 0B (100.00%) - LeetHub

--- swap-salary/swap-salary.sql
+++ swap-salary/swap-salary.sql
@@ -1,6 +1,3 @@
 # Write your MySQL query statement below
-UPDATE Salary
-SET sex = CASE 
-            WHEN sex = 'm' THEN 'f'
-            WHEN sex = 'f' THEN 'm'
-        END;
+UPDATE SALARY
+SET sex = CHAR(ASCII('f')+ASCII('m')-ASCII(sex))
