e3342acc01a3e9d9beb9119b44157e14457bf746
Tarun Yadav <tarunrock2901@gmail.com>
2021-07-07 19:23:54 +0530

    Time: 736 ms (8.65%), Space: 150.7 MB (5.03%) - LeetHub

---
+++ avoid-flood-in-the-city/avoid-flood-in-the-city.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    
+    vector<int> avoidFlood(vector<int>& rains) {
+        vector<int> ans;
+        map<int,int> mp;
+        map<int,int> status;
+        map<int,int> lastRain;
+        for(int i = 0;i<rains.size();i++)
+        {
+            if(rains[i]>0)
+            {
+              mp[rains[i]]++;
+              lastRain[rains[i]] = i;
+            }
+        }
+        
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        
+               
+        for(int i = 0;i<rains.size();i++)
+        {
+            if(rains[i]>0 && status[rains[i]] == 0)
+            {
+                ans.push_back(-1);
+                mp[rains[i]]--;
+                status[rains[i]] = 1;
+                if(mp[rains[i]]>0)
+                {
+                   pq.push(make_pair(lastRain[rains[i]],rains[i]));
+                }
+            }
+            else if(rains[i]>0 && status[rains[i]] == 1)
+            {
+                return {};
+            }
+            else if(rains[i] == 0)
+            {
+                int maxEle = 0;
+                while(pq.size()>0 && mp[pq.top().second]==0 && status[pq.top().second] == 1)
+                {
+                    pq.pop();
+                }
+                if(pq.size()>0)
+                {
+                    int idx = pq.top().first;
+                    int ele = pq.top().second;
+                    if(status[ele] == 1)
+                    {
+                        maxEle = ele;
+                    }
+                    pq.pop();
+                }
+                if(maxEle == 0)
+                {
+                    ans.push_back(1);
+                }
+                else 
+                {   cout<<maxEle<<" ";
+                    ans.push_back(maxEle);
+                    status[maxEle] = 0;
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
