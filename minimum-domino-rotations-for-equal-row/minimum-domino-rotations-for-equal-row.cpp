ba0bc5041d6846c7517a2491afbb10fd24fab001
Tarun Yadav <tarunrock2901@gmail.com>
2021-08-22 23:34:06 +0530

    Time: 112 ms (71.38%), Space: 111.5 MB (78.58%) - LeetHub

--- minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.cpp
+++ minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.cpp
@@ -3,78 +3,85 @@ public:
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
         int n = tops.size();
         
-        unordered_map<int,int> mp;
-        unordered_map<int,int> bot;
+        int a = tops[0];
+        int b = bottoms[0];
+        
+        int countaintop = 0;
+        int countbintop = 0;
+        int countainbot = 0;
+        int countbinbot = 0;
+        
         for(int i = 0;i<n;i++)
         {
-            mp[tops[i]]++;
-            bot[bottoms[i]]++;
-        }
-        
-        bool lb = false;
-        int mn = 0;
-        int ele = 0;
-        int count1 = 0;
-        int count2 = 0;
-        for(auto p:mp)
-        {
-            int val = p.first;
-            int count = p.second;
-            
-            if(bot[val] >= n-count)
+            if(countaintop!=INT_MAX)
             {
-                lb = true;
-                mn = min(count,bot[val]);
-                ele = val;
-                count1 = count;
-                count2 = bot[val];
+                if(tops[i] == a)
+                {
+                    countaintop+= 0;
+                }
+                else if(bottoms[i] == a)
+                {
+                    countaintop+=1;
+                }
+                else
+                {
+                    countaintop = INT_MAX;
+                }
+            }
+             if(countbintop!=INT_MAX)
+            {
+                if(tops[i] == b)
+                {
+                    countbintop+= 0;
+                }
+                else if(bottoms[i] == b)
+                {
+                    countbintop+=1;
+                }
+                else
+                {
+                    countbintop = INT_MAX;
+                }
+            }
+             if(countainbot!=INT_MAX)
+            {
+                if(bottoms[i] == a)
+                {
+                    countainbot+= 0;
+                }
+                else if(tops[i] == a)
+                {
+                    countainbot+=1;
+                }
+                else
+                {
+                    countainbot = INT_MAX;
+                }
+            }
+            if(countbinbot!=INT_MAX)
+            {
+                if(bottoms[i] == b)
+                {
+                    countbinbot+= 0;
+                }
+                else if(tops[i] == b)
+                {
+                    countbinbot+=1;
+                }
+                else
+                {
+                    countbinbot = INT_MAX;
+                }
             }
         }
-        if(lb == false)
+        
+        int mn = min(min(countaintop,countbintop),min(countainbot,countbinbot));
+        if(mn == INT_MAX)
         {
             return -1;
         }
-        int swaps = 0;
-        int val = ele;
-        if(count1>=count2)
-        {
-            for(int i = 0;i<n;i++)
-            {
-                if(tops[i]!=val && bottoms[i] == val)
-                {
-                    swaps++;
-                }
-                if(tops[i] == val && bottoms[i] == val)
-                {
-                    continue;
-                }
-                if(tops[i]!=val && bottoms[i]!=val)
-                {
-                    return -1;
-                }
-            }
-        }
-        else if(count2>count1)
-        {
-            swaps = 0;
-            for(int i = 0;i<n;i++)
-            {
-                if(tops[i] == val && bottoms[i] == val)
-                {
-                    continue;
-                }
-                if(bottoms[i]!=val && tops[i] == val)
-                {
-                    
-                    swaps++;
-                }
-                
-                if(tops[i]!=val && bottoms[i]!=val)
-                {
-                    return -1;
-                }
-            }
-        }
-        return swaps;
+        
+        return mn;
+        
     }
 };
