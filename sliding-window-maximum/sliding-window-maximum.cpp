bc00037a04f7a85b90832caef686380c6ef1c289
Tarun Yadav <tarunrock2901@gmail.com>
2021-08-23 14:58:36 +0530

    Time: 208 ms (98.17%), Space: 138.6 MB (36.60%) - LeetHub

--- sliding-window-maximum/sliding-window-maximum.cpp
+++ sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,28 +1,49 @@
 class Solution {
 public:
-    
-    
-    vector<int> res;
-    
-    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
-            priority_queue<pair<int,int>> pq;
-    
-            int idx = 0;
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> ans;
+        vector<int> prefixMax(nums.size());
+        vector<int>suffixMax(nums.size());
         
-            for(idx = 0;idx<k;idx++)
-            {
-                pq.push(make_pair(arr[idx],idx));
-            }
-            res.push_back(pq.top().first);
+        prefixMax[0] = nums[0];
         
-            for(int i = idx;i<arr.size();i++)
+        for(int i = 1;i<nums.size();i++)
+        {
+            if(i%k == 0)
             {
-                pq.push(make_pair(arr[i],i));
-                while(pq.top().second<=i-k){
-                    pq.pop();
-                }
-                res.push_back(pq.top().first);
+                prefixMax[i] = nums[i];
             }
-            return res;
+            else
+            {    
+                prefixMax[i] = max(prefixMax[i-1],nums[i]);
+            }
+        }
+        
+        
+        suffixMax[nums.size()-1] = nums[nums.size()-1];
+        for(int i = nums.size()-2;i>=0;i--)
+        {
+            if((i+1)%k == 0)
+            {
+                suffixMax[i] = nums[i];
+            }
+            else
+            {
+                suffixMax[i] = max(suffixMax[i+1],nums[i]);
+            }
+        }
+        
+        for(int i = 0;i<=nums.size()-k;i++)
+        {
+            if(i%k == 0)
+            {
+                ans.push_back(suffixMax[i]);
+            }
+            else 
+            {
+                ans.push_back(max(suffixMax[i],prefixMax[i+k-1]));
+            }
+        }
+        return ans;
     }
 };
