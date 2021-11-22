29f6130307425bad68417ffe24706ce5dc98ec04
Tarun Yadav <tarunrock2901@gmail.com>
2021-11-22 16:11:40 +0530

    Time: 53 ms (20.84%), Space: 14.4 MB (20.88%) - LeetHub

---
+++ next-permutation/next-permutation.py
@@ -0,0 +1,31 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        p = -1;
+        for i in range(len(nums)-2,-1,-1):
+            if nums[i]<nums[i+1]:
+                p = i;
+                break;
+        
+        justGreater = -1;
+        for i in range(len(nums)-1,-1,-1):
+            if nums[p]<nums[i]:
+                justGreater = i;
+                break;
+        
+        if p!=-1 and justGreater!=-1:
+            nums[p],nums[justGreater] = nums[justGreater],nums[p];
+        
+        #Reverse
+        
+        j = p+1;
+        k = len(nums) - 1;
+        while(j<k):
+            nums[j],nums[k] = nums[k],nums[j];
+            j+=1;
+            k-=1;
+        
+        
+        
+        
+            
+        
