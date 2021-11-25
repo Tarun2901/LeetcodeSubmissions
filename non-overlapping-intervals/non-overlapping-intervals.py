22897b8c2314cc57501f89aef5c22512ac23cbc3
Tarun Yadav <tarunrock2901@gmail.com>
2021-11-25 18:51:21 +0530

    Time: 1308 ms (79.09%), Space: 53.3 MB (8.87%) - LeetHub

--- non-overlapping-intervals/non-overlapping-intervals.py
+++ non-overlapping-intervals/non-overlapping-intervals.py
@@ -1,6 +1,6 @@
 class Solution:
     def eraseOverlapIntervals(self, intr: List[List[int]]) -> int:
-        intr = sorted(intr,key = lambda x:x[1]);
+        intr = sorted(intr,key = lambda x:x[0]);
         curr = intr[0];
         ans =0
         for i in range(1,len(intr)):
@@ -8,9 +8,9 @@ class Solution:
             nextep = intr[i][1];
             if curr[1]>nextsp:
                 ans+=1
+                if curr[1]>nextep:
+                    curr[1] = nextep;
             else:
                 curr = intr[i];
-     
-    
         return ans
                 
