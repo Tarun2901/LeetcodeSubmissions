2f67d4f82eb2dc226685c22f7ae05221c47e2552
Tarun Yadav <tarunrock2901@gmail.com>
2022-02-02 21:14:55 +0530

    Time: 640 ms (60.30%), Space: 223.9 MB (25.23%) - LeetHub

--- 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.cpp
+++ 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     vector<int> findLonely(vector<int>& nums) {
         unordered_map<int,int> mp;
+        int val;
         vector<int>ans;
         for(int i = 0;i<nums.size();i++)
-        {   int val = nums[i];
+        {  val = nums[i];
             if(mp[val] == 0)
             {
                 mp[val] = 1;
@@ -16,7 +17,7 @@ public:
         }
         for(int i = 0;i<nums.size();i++)
         {
-            int val = nums[i];
+            val = nums[i];
             if(mp[val] == 1 && mp[val+1] == 0 && mp[val-1] == 0)
             {
                 ans.push_back(val);
